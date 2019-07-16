541e37555362cd53210ee75a5281a74c6f114792
Mike <barbeque@users.noreply.github.com>
2019-07-16 11:52:41 -0600

    Move Winforms into its own project, add async task tool, bump .NET to 4.5.2

---
+++ Nondisplayable.Extras/ProcessToTask.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Nondisplayable.Extras
+{
+    /// <summary>
+    /// Extension methods for using an external process as an async task.
+    /// </summary>
+    public static class ProcessToTask
+    {
+        public static async Task<int> WaitForExitAsync(this Process process, CancellationToken cancellationToken)
+        {
+            var tcs = new TaskCompletionSource<int>();
+
+            void OnProcessExited(object sender, EventArgs e)
+            {
+                int exitCode = 0;
+                if(sender is Process)
+                {
+                    exitCode = (sender as Process).ExitCode;
+                }
+                
+                Task.Run(() => tcs.TrySetResult(exitCode));
+            }
+
+            process.EnableRaisingEvents = true;
+            process.Exited += OnProcessExited;
+
+            try
+            {
+                if(process.HasExited)
+                {
+                    return 0;
+                }
+
+                using(cancellationToken.Register(() => Task.Run(() => tcs.TrySetCanceled())))
+                {
+                    return await tcs.Task;
+                }
+            }
+            finally
+            {
+                process.Exited -= OnProcessExited;
+            }
+        }
+    }
+}
